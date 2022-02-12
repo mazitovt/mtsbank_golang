58b332d18c5fd7883e80aea9f0047eb4919bb429
tim <timaz98@ya.ru>
2022-02-13 03:57:13 +0500

    change spelling, move error handler into defer

--- homework_1/bank_deposit/bank_deposit.go
+++ homework_1/bank_deposit/bank_deposit.go
@@ -16,6 +16,12 @@ type kopeck int
 
 func OpenDeposit() (result string, err error) {
 
+	defer func() {
+		if err != nil {
+			err = fmt.Errorf("Открытие вклада невозможно: %v\n", err)
+		}
+	}()
+
 	reader := bufio.NewReader(os.Stdin)
 
 	readInput := func(invitationLine string) string {
@@ -24,21 +30,18 @@ func OpenDeposit() (result string, err error) {
 		return strings.TrimSuffix(input, "\r\n")
 	}
 
-	deposit, e := parseDeposit(readInput("Введите размер депозита (в копейках): "))
-	if e != nil {
-		err = handleOpenDepositError(e)
+	deposit, err := parseDeposit(readInput("Введите размер депозита (в копейках): "))
+	if err != nil {
 		return
 	}
 
-	annualInterest, e := parseAnnualInterest(readInput("Введите размер годового процента (в формате #.#): "))
-	if e != nil {
-		err = handleOpenDepositError(e)
+	annualInterest, err := parseAnnualInterest(readInput("Введите размер годового процента (в формате #.#): "))
+	if err != nil {
 		return
 	}
 
-	timePeriod, e := parseTimePeriod(readInput("Введите срок вклада (в годах): "))
-	if e != nil {
-		err = handleOpenDepositError(e)
+	timePeriod, err := parseTimePeriod(readInput("Введите срок вклада (в годах): "))
+	if err != nil {
 		return
 	}
 
@@ -109,31 +112,35 @@ func parseTimePeriod(strTimePeriod string) (timePeriod year, err error) {
 }
 
 func yearRussianSpelling(year year) (spelling string) {
-	switch year % 10 {
-	case 1:
-		spelling = "год"
-	case 2, 3, 4:
-		spelling = "года"
-	default:
+	if year >= 10 && year <= 20 {
 		spelling = "лет"
+	} else {
+		switch year % 10 {
+		case 1:
+			spelling = "год"
+		case 2, 3, 4:
+			spelling = "года"
+		default:
+			spelling = "лет"
+		}
 	}
 
 	return
 }
 
 func kopeckRussianSpelling(kopeck kopeck) (spelling string) {
-	switch kopeck % 10 {
-	case 1:
-		spelling = "копейка"
-	case 2, 3, 4:
-		spelling = "копейки"
-	default:
+	if kopeck >= 10 && kopeck <= 20 {
 		spelling = "копеек"
+	} else {
+		switch kopeck % 10 {
+		case 1:
+			spelling = "копейка"
+		case 2, 3, 4:
+			spelling = "копейки"
+		default:
+			spelling = "копеек"
+		}
 	}
 
 	return
-}
-
-func handleOpenDepositError(e error) error {
-	return fmt.Errorf("Открытие вклада невозможно: %v\n", e)
 }
